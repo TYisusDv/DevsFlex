c8cca94f5f77b23f0a6b06ffb13c024af34d5b4b
TYisusDV <jesusns1902@gmail.com>
2023-08-10 12:25:08 -0600

    Update

--- pos/models.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
-from config import *
-
-db_mongo_client = MongoClient('mongodb://localhost:27017/')
-db_mongo = db_mongo_client[config_app['db_mongo']['name']]
